05ddf90f86b462822ab26caeee910c5ad2b5b2ba
AgusCorrales <aguscorrales1997@gmail.com>
2024-05-27 12:33:19 +0200

    Actualizacion Pagina

--- ProyectoNoticias/src/context/GlobalState.jsx
+++ ProyectoNoticias/src/context/GlobalState.jsx
@@ -13,7 +13,7 @@ export const GlobalProvider = ({ children }) => {
     const [state, dispatch] = useReducer(NewReducer, initialState);
 
     const getNews = async () => {
-        const res = await axios.get("https://newsapi.org/v2/everything?q=tesla&from=2024-04-26&sortBy=publishedAt&apiKey=84d28dda742546d5a2a71167539093fb");
+        const res = await axios.get("https://newsapi.org/v2/everything?language=en&q=tesla&from=2024-04-26&sortBy=publishedAt&apiKey=84d28dda742546d5a2a71167539093fb");
         console.log(res.data.articles);
         dispatch({
             type: "GET_NEWS",
